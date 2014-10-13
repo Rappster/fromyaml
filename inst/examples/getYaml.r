4f45732910385709a0df1195a8e7440aaea11374
Janko Thyson <janko.thyson@rappster.de>
2014-10-13 18:51:13 +0200

    modified README.md modified examples

--- inst/examples/getYaml.r
+++ inst/examples/getYaml.r
@@ -1,15 +1,15 @@
 \dontrun{
 
 from <- function() {
-  "reactive-ref: {id: x_1, where:.GlobalEnv, as: ref_1}"
+  "reactive-ref: {id: x_1, where: .GlobalEnv, as: ref_1}"
 }
-res <- getYaml(
+yaml <- getYaml(
   from = from,
   ctx = ReactiveReferenceYamlContext.S3()
 )
-res
-res$yaml
-res$index
-res$src
+yaml
+yaml$yaml
+yaml$index
+yaml$src
 
 }
